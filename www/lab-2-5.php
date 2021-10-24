3247c0ade0c3d913fe9dd6ea04868c364da60b43
Takhaev Arseniy <takhaev80@bk.ru>
2021-10-24 20:45:23 +0300

    exercise 1-10 done

---
+++ www/lab-2-5.php
@@ -0,0 +1,31 @@
+<p>Takhaev Arseniy PI-321</p>
+
+<?php
+$i=0; $var=5;
+
+while (++$i<=$var) {
+	echo ($i . ' ');
+}
+echo '<br>';
+
+while (--$var>=0){
+	echo ($var+1 . ' ');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
