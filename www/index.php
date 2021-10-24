b8ddc9af8523cd65edbfe43728e7bd9f296a328e
Takhaev Arseniy <takhaev80@bk.ru>
2021-10-24 14:46:49 +0300

    error correction

--- www/index.php
+++ www/index.php
@@ -8,5 +8,4 @@ echo($d);
 	<li>Link to 1 page: http://php-takhaev/lab-1-1.php</li>
 	<li>Link to 2 page: http://php-takhaev/lab-1-2.php</li>
 	<li>Link to 3 page: http://php-takhaev/lab-1-3.php</li>
-	<li>Link to 4 page: http://php-takhaev/lab-1-4.php</li>
 </ul>
