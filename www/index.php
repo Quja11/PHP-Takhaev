2b5dfd95cfd8595788b96458291511af3903f1f7
Takhaev Arseniy <takhaev80@bk.ru>
2021-10-24 14:43:42 +0300

    exercise 1-5

--- www/index.php
+++ www/index.php
@@ -1,9 +1,12 @@
 <p>Date and time:
 </p>
 <?php
-$d=5;
-echo(gettype($d));
 $d=date("d.m.Y H:i");
 echo($d);
-
 ?>
+<ul>
+	<li>Link to 1 page: http://php-takhaev/lab-1-1.php</li>
+	<li>Link to 2 page: http://php-takhaev/lab-1-2.php</li>
+	<li>Link to 3 page: http://php-takhaev/lab-1-3.php</li>
+	<li>Link to 4 page: http://php-takhaev/lab-1-4.php</li>
+</ul>
