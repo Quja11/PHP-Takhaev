3d306a624069bb5a8922eed545d5912cf1a60e6e
Takhaev Arseniy <takhaev80@bk.ru>
2021-10-24 20:50:54 +0300

    exercise 1-11 done

---
+++ www/lab-2-6.php
@@ -0,0 +1,24 @@
+<p>Takhaev Arseniy PI-321</p>
+
+<?php
+$a = rand(1,10); $b = rand (10,20);
+print ("<p> numbers from segment [".$a.",".$b."]: <br>");
+	while($a<=$b){
+		echo $a . "<br>";
+		$a = ++$a;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
