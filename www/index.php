3f67d3cb29ac72240f4f856133f784959d7b7649
Takhaev Arseniy <takhaev80@bk.ru>
2021-10-24 15:24:26 +0300

    Independent work Number One

--- www/index.php
+++ www/index.php
@@ -5,7 +5,13 @@ $d=date("d.m.Y H:i");
 echo($d);
 ?>
 <ul>
+	<p><i>Part 1</i></p>
 	<li>Link to 1 page: http://php-takhaev/lab-1-1.php</li>
 	<li>Link to 2 page: http://php-takhaev/lab-1-2.php</li>
 	<li>Link to 3 page: http://php-takhaev/lab-1-3.php</li>
+</ul>
+
+<ul>
+	<p><i>Independent work Numbar One</i></p>
+	<li>Link to 4 page: http://php-takhaev/lab1-5.php</li>
 </ul>
