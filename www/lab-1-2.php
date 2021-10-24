178642c203e6e032d3a9abdcc953f9040b7da1d6
Takhaev Arseniy <takhaev80@bk.ru>
2021-10-24 14:09:00 +0300

    exercise 1-3

---
+++ www/lab-1-2.php
@@ -0,0 +1,10 @@
+<p><b>Arithmetic operations</b></p>
+<p>
+<?php
+	$x=rand(1,10);
+	$y=rand(1,10);
+	echo ($x . '+' . $y . '=' . ($x+$y) . '<br>');
+	echo ($x . '-' . $y . '=' . ($x-$y) . '<br>');
+	echo ($x . '*' . $y . '=' . ($x*$y) . '<br>');
+	echo ($x . '/' . $y . '=' . ($x/$y) . '<br>');
+?>
