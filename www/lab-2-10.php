d4e67683afeb1bba34370ab7b023aeb223dff41d
Takhaev Arseniy <takhaev80@bk.ru>
2021-10-25 15:33:58 +0300

    Independent work Number Two, Task 1-2 done

--- www/lab-2-10.php
+++ www/lab-2-10.php
@@ -1,28 +1,60 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>MULTIPLICATION TABLE</title>
+	<title></title>
 </head>
-<body bgcolor="green">
+
+
+<body>
+
+
 <table border="1">
-	<?php
-for ($i=1; $i<=10; $i++){
+
+<?php
+
+for ($i=0; $i<=9; $i++){
 	echo ("<tr>");
+	
+
+
+
+
+
 for ($k=1; $k<=10; $k++){
 	echo ("<td align=center>");
-	$p=$i*$k;
-	echo ($p);
+	$z=($i*10)+$k;
+
+	if($z%2==0){ 
+		echo ('<p style="color:red">' . $z  . '</p>');
+	}else{
+		echo ($z);
+	}
+	
+
+
+
 	echo ("</td>");
 }	
+
+
+
 	echo ("</tr>");
 }
 
 
-	?>
-<p>Takhaev Arseniy PI-321</p>
+
+
+?>
+
 
 
 
 </table>
 </body>
+
+
+
+
+
+
 </html>
