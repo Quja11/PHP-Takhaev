60b4456a73c26f726cc39a83794fa726940b9ead
Takhaev Arseniy <takhaev80@bk.ru>
2021-11-10 19:27:33 +0300

    fix problems

--- www/index.php
+++ www/index.php
@@ -6,7 +6,8 @@ echo($d);
 ?>
 <ul>
 	<p><i>Part 1</i></p>
-	<li>Link to 1 page: takhaev1.y98709yq.beget.tech</li>
+	<li><a href="laba-1-1.y98709yq.beget.tech">Link to 1 page</a> 
+	<li><a href="laba-1-2.y98709yq.beget.tech">Link to 1 page</a> 
 	<li>Link to 2 page: http://php-takhaev/lab-1-2.php</li>
 	<li>Link to 3 page: http://php-takhaev/lab-1-3.php</li>
 </ul>
