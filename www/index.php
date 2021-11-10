30d61a074a3d86c6c135c09c9c4d203163bf42f9
Takhaev Arseniy <takhaev80@bk.ru>
2021-11-10 20:47:30 +0300

    del archive, fix problems

--- www/index.php
+++ www/index.php
@@ -7,40 +7,40 @@ echo($d);
 ?>
 <ul>
 	<p><i><b>Part 1 (php introduction)</b></i></p>
-	<li><a href="http://php-takhaev/index.php">Link to Main Page (Exercise 1-1 and 1-5)</a>
+	<li><a href="index.php">Link to Main Page (Exercise 1-1 and 1-5)</a>
 
-	<li><a href="http://php-takhaev/lab-1-1.php">Link to Exercise 1-2</a>
+	<li><a href="lab-1-1.php">Link to Exercise 1-2</a>
 
-	<li><a href="http://php-takhaev/lab-1-2.php">Link to Exercise 1-3</a>
+	<li><a href="lab-1-2.php">Link to Exercise 1-3</a>
 
-	<li><a href="http://php-takhaev/lab-1-3.php">Link to Exercise 1-4</a>
+	<li><a href="lab-1-3.php">Link to Exercise 1-4</a>
 </ul>
 
 <ul>
 	<p><i><b>Independent work Number One</b></i></p>
-	<li><a href="http://php-takhaev/lab1-5.php">Link to Task #1-1
+	<li><a href="lab1-5.php">Link to Task #1-1
 	</a>
 </ul>
 
 <ul>
 	<p><i><b>Part 2 (control structures)</b></i></p>
-	<li><a href="http://php-takhaev/lab-2-1.php">Link to Exercise 1-6</a>
+	<li><a href="lab-2-1.php">Link to Exercise 1-6</a>
 
-	<li><a href="http://php-takhaev/lab-2-2.php">Link to Exercise 1-7</a>
+	<li><a href="lab-2-2.php">Link to Exercise 1-7</a>
 
-	<li><a href="http://php-takhaev/lab-2-3.php">Link to Exercise 1-8</a>
+	<li><a href="lab-2-3.php">Link to Exercise 1-8</a>
 
-	<li><a href="http://php-takhaev/lab-2-4.php">Link to Exercise 1-9</a>
+	<li><a href="lab-2-4.php">Link to Exercise 1-9</a>
 
-	<li><a href="http://php-takhaev/lab-2-5.php">Link to Exercise 1-10</a>
+	<li><a href="lab-2-5.php">Link to Exercise 1-10</a>
 
-	<li><a href="http://php-takhaev/lab-2-6.php">Link to Exercise 1-11</a>
+	<li><a href="lab-2-6.php">Link to Exercise 1-11</a>
 
-	<li><a href="http://php-takhaev/lab-2-7.php">Link to Exercise 1-12</a>
+	<li><a href="lab-2-7.php">Link to Exercise 1-12</a>
 
-	<li><a href="http://php-takhaev/lab-2-8.php">Link to Exercise 1-13</a>
+	<li><a href="lab-2-8.php">Link to Exercise 1-13</a>
 
-	<li><a href="http://php-takhaev/lab-2-9.php">Link to Exercise 1-14</a>	
+	<li><a href="lab-2-9.php">Link to Exercise 1-14</a>	
 </ul>
 
 <ul>
@@ -48,7 +48,7 @@ echo($d);
 	<li><a href="lab-2-10.php">Link to Task #1-2
 	</a>
 
-	<li><a href="http://php-takhaev/lab-2-11.php">Link to Task #1-3
+	<li><a href="lab-2-11.php">Link to Task #1-3
 	</a>
 </ul>
 
