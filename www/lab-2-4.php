0a07731010b98178fed50e734b7e9a7389554432
Takhaev Arseniy <takhaev80@bk.ru>
2021-11-13 20:30:28 +0300

    Independent work, Task 2.4 done

---
+++ www/lab-2-4.php
@@ -0,0 +1,28 @@
+<p>Takhaev Arseniy PI-321</p>
+
+<?php
+//Самостоятельная работа, вариант 4
+$aqua = array();
+$N = rand();
+$count = 0;
+$ds = 0;
+
+	for($m=0; $m<=$N; $m++){
+		$aqua[$m] = rand(-32768,32767);
+		echo $aqua[$m] . " ";
+
+		if($aqua[$m] > 0) $count++;
+		else $count = 0;
+		
+		if ($count > $ds) $ds++;
+
+	}
+echo (" <br> The maximum number of consecutive positive: " . $ds);
+
+
+
+
+
+
+
+?>
