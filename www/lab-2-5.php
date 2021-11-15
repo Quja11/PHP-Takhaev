12e2d993cb3c25744f9f0ac03859099dbd26ed7e
Takhaev Arseniy <takhaev80@bk.ru>
2021-11-15 16:54:41 +0300

    Part 2. User functions: Task 2.5 done

---
+++ www/lab-2-5.php
@@ -0,0 +1,27 @@
+<p>Takhaev Arseniy PI-321</p>
+
+<?php
+//Task 2.5
+
+function abc($u,$t){
+	if ($u >= 0 and $t >= 0) return ($u);
+	if ($u < 0 and $t >= 0) return ($t);
+	if ($u >= 0 and $t < 0) return ($u - 2*$t);
+	if ($u < 0 and $t < 0) return ($u*$t + 3*$t);              
+}
+
+$a = rand(-100,100);
+$b = rand(-100,100);
+echo ("a = " . $a . " b = " . $b . "<br>");
+
+$u = abc($a - $b*$b, $b - $a);
+$t = abc($a, $b - $a*$a);
+
+echo ("1 function result:" . $u);
+echo ("<br> 2 function result:" . $t);
+
+
+$z = $u + $t;
+echo ("<br> The value of z is $z");
+
+?>
