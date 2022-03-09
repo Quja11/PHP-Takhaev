b0faedd36e36e3aed64c783b6b547351400ac096
Quja11 <takhaev80@bk.ru>
2022-03-09 16:53:44 +0300

    files

---
+++ bd_user/index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+	<title>Сведения о прользователях сайта</title>
+</head>
+<body>
+
+<?php
+ $link = mysqli_connect("localhost", "root", "", "users") or die ("Невозможно подключиться к серверу");// установление соединения с сервером
+
+ mysqli_set_charset($link, "utf8");
+
+ if($link == false) echo "irr conn";
+ else echo "reg conn";
+?>
+
+<h2>Зарегистрированные пользователи:</h2>
+<table border="1">
+<tr> 
+ <th>Имя</th> 
+ <th>E-mail</th>
+ <th>Редактировать</th>
+ <th>Уничтожить</th> 
+</tr>
+
+<?php
+
+$result=mysqli_query($link, "SELECT id_user, user_name, user_e_mail FROM user"); // запрос на выборку сведений о пользователях
+
+while ($row=mysqli_fetch_array($result)){// для каждой строки из запроса
+ echo "<tr>";
+
+ echo "<td>" . $row['user_name'] . "</td>";
+ echo "<td>" . $row['user_e_mail'] . "</td>";
+
+ echo "<td><a href='edit.php?id=" . $row['id'] . "'>Редактировать</a></td>"; // запуск скрипта для редактирования
+ echo "<td><a href='delete.php?id=" . $row['id'] . "'>Удалить</a></td>"; // запуск скрипта для удаления записи
+
+ echo "</tr>";
+}
+print "</table>";
+
+$num_rows = mysqli_num_rows($result); // число записей в таблице БД
+print("<P>Всего пользователей: $num_rows </p>");
+?>
+<p><a href="new.php">Добавить пользователя</a>
+
+</body>
+</html>
